2a8fd17b2105c16a7e0c8785ab753cba19055589
Ji Zhang <ji.zhang@samsung.com>
2019-08-01 13:04:07 +0800

    update

--- es_add_multi_fields.sh
+++ es_add_multi_fields.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -u
 
 curl -X PUT -H "Content-Type: application/json" \
-     -d '{"properties":{"gs.app":{"type":"text","fields":{"raw":{"type":"keyword"}}}}}' \
-     http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.08.01/_mapping
+     -d '{"mappings":{"properties":{"gs.app":{"type":"text","fields":{"raw":{"type":"keyword"}}}}}}' \
+     http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.07.31
+
+#curl -X PUT -H "Content-Type: application/json" \
+#     -d '{"properties":{"gs.app":{"type":"text","fields":{"raw":{"type":"keyword"}}}}}' \
+#     http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.08.01/_mapping
