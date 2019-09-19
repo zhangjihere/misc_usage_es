a920aefededdf3adb6c1c21b37720c5a4d28a467
Ji Zhang <zhangjihere@hotmail.com>
2019-09-19 18:40:41 +0800

    update the misc_usage_es

--- es_add_multi_fields.sh
+++ es_add_multi_fields.sh
@@ -7,3 +7,8 @@ curl -X PUT -H "Content-Type: application/json" \
 #curl -X PUT -H "Content-Type: application/json" \
 #     -d '{"properties":{"gs.app":{"type":"text","fields":{"raw":{"type":"keyword"}}}}}' \
 #     http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.08.01/_mapping
+
+
+curl -X PUT -H "Content-Type: application/json" -d '{"mappings":{"properties":{"gu.response.result":{"type":"object"}}}}' http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.09.16
+
+curl -X DELETE http://ES-1.FaaSProxy.srcb.ceres.local:9200/couchdb-ow-2019.09.16
